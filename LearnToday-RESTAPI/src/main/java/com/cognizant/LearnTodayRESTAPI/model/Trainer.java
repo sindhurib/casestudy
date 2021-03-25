3d7051d2c3fd0b511a2a9a80ccc9a59311417517
casestudy <sindhuribayappu11@gmail.com>
2021-03-25 13:09:51 +0530

    casestudy

---
+++ LearnToday-RESTAPI/src/main/java/com/cognizant/LearnTodayRESTAPI/model/Trainer.java
@@ -0,0 +1,21 @@
+package com.cognizant.LearnTodayRESTAPI.model;
+
+public class Trainer {
+	private int trainerId;
+	private String password;
+	public int getTrainerId() {
+		return trainerId;
+	}
+	
+	public void setTrainerId(int trainerId) {
+		this.trainerId = trainerId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
